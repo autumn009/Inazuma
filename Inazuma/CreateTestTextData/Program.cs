883b8b363d53a349023a2f0bbd7028bc9628ca4d
Akira Kawamata <autumn@piedey.co.jp>
2022-09-24 09:43:30 +0900

    add new

---
+++ Inazuma/CreateTestTextData/Program.cs
@@ -0,0 +1,30 @@
+﻿// read resource
+
+using System.Reflection;
+
+const string outputDir = @"c:\delme\testdata";
+
+var words = getWords();
+Console.WriteLine(words.Length);
+
+
+string[] getWords()
+{
+    var words = Assembly.GetExecutingAssembly().GetManifestResourceStream("CreateTestTextData.words.txt");
+    if (words == null) throw new Exception("words not found");
+
+    var wordList = new List<string>();
+    var reader = new StreamReader((Stream)words);
+    for (; ; )
+    {
+        var s = reader.ReadLine();
+        if (s == null) break;
+        wordList.Add(s);
+    }
+    return wordList.ToArray();
+}
+
+
+
+
+
